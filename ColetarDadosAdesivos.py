7a04122827599190f6de4ec5edd5d89a1c769206
ManoelSobrinho <cleonaldosobrinho@hotmail.com>
2023-08-04 22:56:06 -0300

    Add files via upload

--- ColetarDadosAdesivos.py
+++ ColetarDadosAdesivos.py
@@ -56,6 +56,12 @@ for stk in stk_regulars:
     # Leitura e tratamento dos dados
 
     for sticker in stickers:
+        link = str(sticker.find('a'))
+        padrao = re.compile('<(.*?)>')
+        achado = padrao.match(str(link))
+        link = achado.group(1)
+        link = link[8:-1]
+
         name = sticker.find('div', attrs={'class': 'well result-box nomargin'})
         name = name.find('h3')
         name = name.text
@@ -72,17 +78,29 @@ for stk in stk_regulars:
         listings = re.sub('[^0-9]', '', listings)
         listings = listings.replace('\n', '')
 
-        sticker_list.append([name, price, listings])
+        # Acessa o link do sticker e coleta volume
 
-#print(sticker_list)
+        url = link
+        response = requests.get(url)
+        content = response.content
+        site = BeautifulSoup(content, 'html.parser')
+
+        volume = site.select('td')[2]
+        volume = volume.text
+        volume = re.sub('[^0-9]', '', volume)
+        volume = volume.replace('\n', '')
+
+        sticker_list.append([name, price, listings, volume])
+
+print(sticker_list)
 
 # Colocar em um dataframe e salvar em arquivo
 
-df_regular_sticker = pd.DataFrame(sticker_list, columns = ['name', 'price', 'listings'])
-df_regular_sticker.to_excel('data_regulars_stickers.xlsx', index = False)
+#df_regular_sticker = pd.DataFrame(sticker_list, columns = ['name', 'price', 'listings'])
+#df_regular_sticker.to_excel('data_regulars_stickers.xlsx', index = False)
 
 # Loop para percorrer os links dos stickers de torneio
-
+'''
 sticker_list = []
 
 for stk in stk_tournaments:
@@ -117,6 +135,8 @@ for stk in stk_tournaments:
         price = price.text
         price = price[4:]
         price = price.replace('.', '').replace(',', '.')
+        price = price.replace('\n', '')
+        price = price.replace('Recent Price', '')
 
         listings = sticker.find('a', attrs={'class': 'btn btn-default market-button-item'})
         listings = listings.text
@@ -129,5 +149,6 @@ for stk in stk_tournaments:
 
 # Colocar em um dataframe e salvar em arquivo
 
-df_tournament_sticker = pd.DataFrame(sticker_list, columns = ['name', 'tournament', 'price', 'listings'])
-df_tournament_sticker.to_excel('data_tournament_stickers.xlsx', index = False)
+#df_tournament_sticker = pd.DataFrame(sticker_list, columns = ['name', 'tournament', 'price', 'listings'])
+#df_tournament_sticker.to_excel('data_tournament_stickers.xlsx', index = False)
+'''
